aa1f41ba370d0e7e6e2b5591b25db1deda3d1f67
AnabelenScript <152194607+AnabelenScript@users.noreply.github.com>
2024-06-30 17:13:40 -0600

    Update Paneles1.jsx

--- Landing/src/components/Molecules/Paneles1.jsx
+++ Landing/src/components/Molecules/Paneles1.jsx
@@ -18,16 +18,16 @@ function Paneles1(){
         <div className="panel">
               <Panel/>
               <div class="card-body">
-                <h5 class="card-title">BATERIA DE LITIO</h5>
-                <p class="card-text">Huawei LUNA2000-5KW-CO $45,000</p><br />
+                <h5 class="card-title">Panel Monocristalino</h5>
+              <p class="card-text">SUNOVA SOLAR de 460W 0.225 DLLS/W $2,500</p>
                 <Button/>
               </div>
             </div>
             <div class="panel">
              <Panel/>
               <div class="card-body">
-                <h5 class="card-title">INVERSOR</h5>
-                <p class="card-text">Huawei SUN2000-3KTL-L1 $17,000</p><br />
+                <h5 class="card-title">Panel Fotovoltaico</h5>
+              <p class="card-text">Monocristalino CNS 555W 0.200 UDS/W $3,000</p>
               <Button/>
               </div>
             </div>
